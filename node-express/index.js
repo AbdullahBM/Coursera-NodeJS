a0ae7664e41c99e773dc7920644d7e0fee8e3c6c
abdullah-cc <abdullah@cloudcoherence.io>
2020-10-29 17:35:24 +0500

    update

--- node-express/index.js
+++ node-express/index.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const http = require("http");
-const morgan = require('morgan');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.json());
 
-app.use(express.static(`${__dirname}/public`));
+// Mount the Router so any request coming to dishes or promotions or leader will be handled by dishRouter, promoRouter, and leaderRouter
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
+app.use(express.static(__dirname + "/public"));
+
+// set up server
 app.use((req, res, next) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<html><body><h1>Hello, World!</h1></body></html>');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
 
-const server = http.createServer(app)
+// create server
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}`);
-  });
+  console.log(`Server running at http://${hostname}:${port}`);
+});
